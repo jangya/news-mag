1c3f46eeb1fe078273e1f42b42b88448324ea94a
i354110 <jangya.narayana.satapathy@sap.com>
2021-03-29 22:36:06 +0530

    support heroku

--- server/config/index.js
+++ server/config/index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const env = process.env.NODE_ENV || 'staging';
+const port = process.env.PORT || 5000;
 const resolve = require('path').resolve;
 
 // env specific config
 const cfg = require('./env/' + env);
 cfg.env = env;
+cfg.port = port;
 
 cfg.clientDir = resolve(__dirname, '../../client/build');
 
